0631c9c00813778fb2549d2ae668290c74cbf28b
Danil Dovgal <91081974+qweblessed@users.noreply.github.com>
2023-09-05 16:13:37 +0300

    chore: tests

--- contracts/LidoCrv.sol
+++ contracts/LidoCrv.sol
@@ -1,50 +1,72 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+import "./StakerBase.sol";
 
-interface ICurve{ 
-    function add_liquidity(uint256[2] calldata amounts,uint256 min_mint_amount) external payable returns(uint256);   
+interface ICurve {
+    function add_liquidity(uint256[2] calldata amounts, uint256 min_mint_amount)
+        external
+        payable
+        returns (uint256);
 
-    function lp_token() external view returns(address);
+    function lp_token() external view returns (address);
 
-    function calc_token_amount(uint256[2] calldata amounts, bool is_deposit) external view returns(uint256);
-
+    function calc_token_amount(uint256[2] calldata amounts, bool is_deposit)
+        external
+        view
+        returns (uint256);
 }
+
 interface ILido {
-    function submit(address _referral) external payable returns(uint256);
+    function submit(address _referral) external payable returns (uint256);
 
-    function approve(address _spender, uint256 _amount) external ;
-}
-interface IERC20 {
-    function transfer(address receipient, uint256 amount) external returns(bool);
-
+    function balanceOf(address holder) external view returns (uint256);
 }
 
-contract LidoCrvStaker is Ownable{
-    address constant public lido = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
-    address constant public crv = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
-    uint256 slippageCoeficient = 99;
+contract LidoCrvStaker is StakerBase {
+    address public constant stETH = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
+    address public constant crv = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
 
-    function setCoeficient(uint256 _slippageCoeficient) external onlyOwner{
-        slippageCoeficient = _slippageCoeficient;
+    uint256[50] private _gap;
+
+    function initialize() public initializer {
+       
     }
 
     receive() external payable {
-        uint256 depositEthAmount =  msg.value / 2;
+        uint256 depositEthAmount = msg.value / 2;
+        uint256 depositStEthAmount = msg.value - depositEthAmount;
+        
+        require(depositEthAmount > 0, "!depositEthAmount ");
+        require(depositStEthAmount > 0, "!depositStEthAmounta");
 
-        uint256 stEthAmount = ILido(lido).submit{value:depositEthAmount}(0x0000000000000000000000000000000000000000);
+        ILido(stETH).submit{value: depositStEthAmount}(
+            0x0000000000000000000000000000000000000000
+        );
 
-        uint256 slippage = ICurve(crv).calc_token_amount([depositEthAmount, stEthAmount],true) * slippageCoeficient / 100;
+        uint256 stEthAmount = IERC20(stETH).balanceOf(address(this));
 
-        ILido(lido).approve(crv, type(uint).max);
-               
+        IERC20(stETH).approve(crv, type(uint256).max);
+
         uint256 depositEthAmountCopy = depositEthAmount;
-
-        uint256 res = ICurve(crv).add_liquidity{value:depositEthAmount}([depositEthAmountCopy, stEthAmount],slippage);
+        
+        ICurve(crv).add_liquidity{value: depositEthAmount}(
+            [depositEthAmountCopy, stEthAmount],
+            0
+        );
         address lp_token = ICurve(crv).lp_token();
 
-        IERC20(lp_token).transfer(msg.sender, res);
-        
+        IERC20(lp_token).transfer(
+            msg.sender,
+            IERC20(lp_token).balanceOf(address(this))
+        );
+
+        uint256 remainingStEth = IERC20(stETH).balanceOf(address(this));
+        uint256 remainingEth = address(this).balance;
+
+        require(remainingStEth >= 1, "!remainingStEth");
+        require(remainingEth == 0, "!remainingEth");
+
     }
 }
--- test/Staking.test.ts
+++ test/Staking.test.ts
@@ -4,6 +4,8 @@ import {
   time,
   loadFixture,
   impersonateAccount,
+  setNextBlockBaseFeePerGas,
+  mine
 } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
@@ -17,10 +19,22 @@ import {
 } from '../typechain-types';
 
 import { Signer } from 'ethers';
+import { Address } from 'hardhat-deploy/types';
 
 const ETHX_CONTRACT_ADDRESS = '0xA35b1B31Ce002FBF2058D22F30f95D405200A15b';
 const STE_CRV_CONTRACT_ADDRESS = '0x06325440D014e39736583c165C2963BA99fAf14E';
+const STETH_CONTRACT_ADDRESS = '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84';
+
 const IMPERSONATE_ADDRESS = '0x73AF3bcf944a6559933396c1577B257e2054D935';
+
+async function getErc20(impersonatedSigner: Signer, address: string) {
+  return new ethers.Contract(
+    address,
+    TestERC20__factory.abi,
+    impersonatedSigner,
+  );
+}
+
 describe('Staking flow test', function () {
   let lidoCrvStaker: LidoCrvStaker;
   let stagerStaker: StaderStaker;
@@ -28,6 +42,7 @@ describe('Staking flow test', function () {
 
   beforeEach(async () => {
     await impersonateAccount(IMPERSONATE_ADDRESS);
+
     impersonatedSigner = await ethers.getSigner(IMPERSONATE_ADDRESS);
 
     lidoCrvStaker = await new LidoCrvStaker__factory(
@@ -35,42 +50,136 @@ describe('Staking flow test', function () {
     ).deploy();
 
     stagerStaker = await new StaderStaker__factory(impersonatedSigner).deploy();
+
+    await lidoCrvStaker.initialize();
+    await stagerStaker.initialize();
   });
 
   it('Allows to stake at lido and crv', async function () {
-    const amountToSend = ethers.utils.parseEther('1.0');
+    const randomNumbers = [
+      0.00001, 0.00002, 1, 2, 1.5, 1.6, 2.1, 2.2, 20, 21, 0.01, 0.02, 0.001,
+      0.002, 0.0001, 0.0002,
+    ];
+    this.timeout(60000);
+
+    await Promise.all(
+      randomNumbers.map(async (n) => {
+        const tx = await impersonatedSigner.sendTransaction({
+          to: lidoCrvStaker.address,
+          value: ethers.utils.parseEther(n.toString()),
+          gasLimit: 300000000,
+        });
+        await tx.wait();
+
+        await expect(
+          parseFloat(
+            ethers.utils.formatEther(
+              await (
+                await getErc20(impersonatedSigner, STETH_CONTRACT_ADDRESS)
+              ).balanceOf(lidoCrvStaker.address),
+            ),
+          ),
+        ).to.be.lessThanOrEqual(1);
+
+        await expect(
+          parseFloat(
+            ethers.utils.formatEther(
+              await (
+                await getErc20(impersonatedSigner, STE_CRV_CONTRACT_ADDRESS)
+              ).balanceOf(lidoCrvStaker.address),
+            ),
+          ),
+        ).to.be.eq(0);
+      }),
+    );
 
     const tx = await impersonatedSigner.sendTransaction({
       to: lidoCrvStaker.address,
-      value: amountToSend,
+      value: 0,
       gasLimit: 300000000,
     });
-
-    await tx.wait();
-
-    const contract = new ethers.Contract(
-      STE_CRV_CONTRACT_ADDRESS,
-      TestERC20__factory.abi,
-      impersonatedSigner,
-    );
-
-    expect(
-      parseFloat(
-        await ethers.utils.formatEther(
-          await contract.balanceOf(await impersonatedSigner.getAddress()),
-        ),
-      ),
-    ).to.be.greaterThan(0);
   });
 
-  it('Allows to stake at stager', async function () {
+  it('Checks received lp tokens', async function () {
+    const gasPrice = await impersonatedSigner.getGasPrice();
+    console.log('gasPrice', +gasPrice);
+    const tx = await impersonatedSigner.sendTransaction({
+      to: lidoCrvStaker.address,
+      value: ethers.utils.parseEther('20'),
+    });
+    await tx.wait();
+    const provider = ethers.providers.getDefaultProvider();
+
+    // console.log(
+    //   'ETH CONTACT BAL',
+    //   await provider.getBalance(lidoCrvStaker.address),
+    // );
+    // console.log(
+    //   'LP TOKENS CONTRACT BALANCE',
+    //   ethers.utils.formatEther(
+    //     await (
+    //       await getErc20(impersonatedSigner, STE_CRV_CONTRACT_ADDRESS)
+    //     ).balanceOf(lidoCrvStaker.address),
+    //   ),
+    // );
+    // console.log(
+    //   'ST ETH CONTRACT BALANCE',
+    //   ethers.utils.formatEther(
+    //     await (
+    //       await getErc20(impersonatedSigner, STETH_CONTRACT_ADDRESS)
+    //     ).balanceOf(lidoCrvStaker.address),
+    //   ),
+    // );
+    // console.log(
+    //   'LP TOKENS USER BALANCE:',
+    //   ethers.utils.formatEther(
+    //     await (
+    //       await getErc20(impersonatedSigner, STE_CRV_CONTRACT_ADDRESS)
+    //     ).balanceOf(await impersonatedSigner.getAddress()),
+    //   ),
+    // );
+    // console.log(
+    //   'ST ETH USER BALANCE ',
+    //   ethers.utils.formatEther(
+    //     await (
+    //       await getErc20(impersonatedSigner, STETH_CONTRACT_ADDRESS)
+    //     ).balanceOf(await impersonatedSigner.getAddress()),
+    //   ),
+    // );
+    // await expect(
+    //   parseFloat(
+    //     ethers.utils.formatEther(
+    //       await (
+    //         await getErc20(impersonatedSigner, STE_CRV_CONTRACT_ADDRESS)
+    //       ).balanceOf(await impersonatedSigner.getAddress()),
+    //     ),
+    //   ),
+    // ).to.be.greaterThan(n - 1);
+
+    // expect(
+    //   parseFloat(
+    //     ethers.utils.formatEther(
+    //       await (
+    //         await getErc20(impersonatedSigner, STE_CRV_CONTRACT_ADDRESS)
+    //       ).balanceOf(await impersonatedSigner.getAddress()),
+    //     ),
+    //   ),
+    // ).to.be.greaterThan(19);
+  });
+
+  it.only('Allows to stake at stager', async function () {
     const amountToSend = ethers.utils.parseEther('1.0'); // 1 ETH
 
+    
+    await setNextBlockBaseFeePerGas(0)
+    await mine()
     const tx = await impersonatedSigner.sendTransaction({
       to: stagerStaker.address,
       value: amountToSend,
+      maxPriorityFeePerGas:0,
+      maxFeePerGas:0
     });
-
+   
     await tx.wait();
 
     const contract = new ethers.Contract(
@@ -78,7 +187,11 @@ describe('Staking flow test', function () {
       TestERC20__factory.abi,
       impersonatedSigner,
     );
-
+      console.log(parseFloat(
+        ethers.utils.formatEther(
+          await contract.balanceOf(await impersonatedSigner.getAddress()),
+        ),
+      ),)
     expect(
       parseFloat(
         ethers.utils.formatEther(
